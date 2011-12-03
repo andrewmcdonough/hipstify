aa5130c6162973c6988f2f0b394abc13fb3d2449
Andrew McDonough <andrew@andrewmcdonough.co.uk>
2011-12-03 21:46:07 +0000

    App now successfully gets reviews

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  #protect_from_forgery
+
+  before_filter :set_headers
+
+  def set_headers
+    response.headers["Access-Control-Allow-Origin"] = "*"
+  end
+
 end
+
--- spotify/javascripts/app.js
+++ spotify/javascripts/app.js
@@ -1,2 +1,18 @@
-document.getElementByID("content").innerHTML = "qwerqwer";
+jQuery(document).ready(function($) {
+  console.log("hello");
+  $("#content").html("Testing jQuery");
+  $.get("http://spotview.dev/reviews.json", function(data) {
+    html = "";
+    console.log(data);
+    html += "<ul>";
+    for (row in data) {
+      html += "<li>"
+      html += data[row]["artist"];
+      html += "</li>" 
+    }; 
+    html += "</ul>";
+    $("#content").html(html);
+  });
+});
+
 
