292a5b16b7ff92b3608dcf0b6c87d48be0c6ac17
Andrew McDonough <andrew@andrewmcdonough.co.uk>
2011-12-03 11:27:02 +0000

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spotview::Application.config.secret_token = 'd5e9a0fd66a01c2028c4b162d464fe1d9a33ce8b19791ad4a78346bd78d38f53cb5b6e74d486ddd9c72aefbd605f325b6bbbf6e9b9b453e58fb8f7d150580d9c'
