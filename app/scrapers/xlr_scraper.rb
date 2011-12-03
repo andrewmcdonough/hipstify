a56af354f003809ddb3c4a362aa94acb1a96e01e
Andrew McDonough <andrew@andrewmcdonough.co.uk>
2011-12-03 18:41:13 +0000

    Added XLR8R parser for reviews

--- app/scrapers/xlr_scraper.rb
+++ app/scrapers/xlr_scraper.rb
@@ -8,7 +8,7 @@ class XlrScraper
     list = []
     doc.css("h2.title").each do |e|
       href = e.children.first["href"]
-      url = "http://www.xlr8r.com/#{href}"
+      url = "http://www.xlr8r.com#{href}"
       title = e.text
       list << {
         :url => url,
@@ -24,10 +24,10 @@ class XlrScraper
     review = OpenStruct.new
     fh = open(url)
     doc = Nokogiri::XML(fh)
-    review.content = doc.css(".editorial").first.text
-    review.rating = doc.css(".mark .value").first.text.to_f
-    review.artist = doc.css("h1.fn").children[0].text
-    review.recording = doc.css("h1.fn").children[3].text
+    review.content = doc.css(".post p").text rescue nil
+    review.rating = doc.css(".post-details li").children[7].text.split("/").first.to_f rescue nil
+    review.artist = doc.css("h1").children[1].text.strip rescue nil
+    review.recording = doc.css("h1").children[2].text.strip rescue nil
     fh.close
     review
   end
