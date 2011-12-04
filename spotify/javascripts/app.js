c7063251a50cb2a40c437d671e1d724cd927e030
Andrew McDonough <andrew@andrewmcdonough.co.uk>
2011-12-04 11:31:29 +0000

    Added hipsify banner and review colours

--- spotify/javascripts/app.js
+++ spotify/javascripts/app.js
@@ -4,16 +4,17 @@ jQuery(document).ready(function($) {
   $.get("http://spotview.dev/reviews.json", function(data) {
     html = "";
     console.log(data);
-    html += "<table width='100%'>";
+    html += "<table width='100%' class='listing'>";
     html += "<tr><th>Artist</th><th>Album</th><th>Blog</th><th>Rating</th>";
     for (row in data) {
       artist_link = data[row]["artist_spotify_uri"];
       recording_link = data[row]["recording_spotify_uri"];
+      var rating_color = data[row]["rating_color"];
       html += "<tr>"
       html += "<td><a href='"+artist_link+"'>"+data[row]["artist"]+"</a></td>";
       html += "<td><a href='"+recording_link+"'>"+data[row]["recording"]+"</a></td>";
       html += "<td>"+data[row]["blog_title"]+"</td>";
-      html += "<td>"+data[row]["rating"]+"</td>";
+      html += "<td><span class='rating-"+rating_color+"'>"+data[row]["rating"]+"</span></td>";
       html += "</tr>" 
     }; 
     html += "</table>";
